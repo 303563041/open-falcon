e07447677008d26553151cd07fc6f175c9b31530
liang.chen <liang.chen@funplus.com>
2018-03-27 12:03:31 +0800

    fix bug

--- get_alarm.py
+++ get_alarm.py
@@ -17,7 +17,7 @@ DUTY_API = "http://duty.funplus.io/Event/index"
 DUTY_SPACE_UUID = "xxxxxxxxxxx"
 DUTY_TOKEN = "xxxxxxxxxxxxxxxxx"
 DUTY_CATEGORY = "monitor"
-DUTY_DEFAULT_STATUS = 2  # 2 stands for critical
+#DUTY_DEFAULT_STATUS = 2  # 2 stands for critical
 d = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
 def conn():
@@ -31,8 +31,8 @@ def conn():
         logger.error("%s Fail to connection redis: %s" % (d, e))
     return r
 
-def call_duty(html_str, service, tags,
-              space_uuid=DUTY_SPACE_UUID, token=DUTY_TOKEN, category=DUTY_CATEGORY, status=DUTY_DEFAULT_STATUS):
+def call_duty(html_str, service, tags, status,
+              space_uuid=DUTY_SPACE_UUID, token=DUTY_TOKEN, category=DUTY_CATEGORY):
     data = {
         "space_uuid": space_uuid,
         "token": token,
@@ -49,11 +49,23 @@ def call_duty(html_str, service, tags,
             if not flag:
                 rs = requests.post(DUTY_API, data=data)
                 logger.info("%s Call duty succeed!" % d)
+                logger.info("%s %s" % (d, data))
                 flag = True
         except Exception, e:
             logger.error("%s Failed to call duty api: %s" % (d, e.message))
             flag = False
     return flag
+
+def format_msg(info):
+    ''' Format the log information. '''
+    tg = ""
+    for k, v in info["strategy"]["tags"].items():
+        tg += "%s=%s," % (k, v)
+    msg = "Expr(%s/%s, %s%s%s); the %s/%s current value is %s; This event has lasted %s time" % (info["strategy"]["metric"], tg.strip(','), info["strategy"]["func"], info["strategy"]["operator"], info["strategy"]["rightValue"], info["strategy"]["metric"], tg.strip(','), info["leftValue"], info["currentStep"])
+    service = info["strategy"]["metric"]
+    tags = "townkins:%s" % info["endpoint"]
+    return msg, tags, service
+
 
 def main():
     '''
@@ -67,23 +79,27 @@ def main():
                 for value in r.lrange(key, 0, -1):
                     info = json.loads(value)
                     if info["status"] == "PROBLEM":
-                        id = info
-                        tg = ""
-                        for k, v in info["strategy"]["tags"].items():
-                            tg += "%s=%s," % (k, v)
-                        msg = "Expr(%s/%s, %s%s%s); the %s/%s current value is %s; This event has lasted %s time" % (info["strategy"]["metric"], tg.strip(','), info["strategy"]["func"], info["strategy"]["operator"], info["strategy"]["rightValue"], info["strategy"]["metric"], tg.strip(','), info["leftValue"], info["currentStep"])
-                        service = info["strategy"]["metric"]
-                        tags = "townkins:%s" % info["endpoint"]
+                        status = 2
+                        msg, tags, service = format_msg(info)
 
-                        #send alarm msg to duty
-                        flag = call_duty(msg, service, tags)
+                        # send alarm error msg to duty
+                        flag = call_duty(msg, service, tags, status)
 
-                        #del values
+                        # del error msg
                         if flag:
                             r.lrem(key, value, 1)
-                            logger.info("%s del alarm msg, key: %s, value: %s" % (d, key, value))
+                            logger.info("%s del Problem alarm msg, key: %s, value: %s" % (d, key, value))
+
                     elif info["status"] == "OK":
-                        r.lrem(key, value, 1)
+                        status = 0
+                        msg, tags, service = format_msg(info)
+                        # send alarm OK msg to duty
+                        flag = call_duty(msg, service, tags, status)
+
+                        # del ok msg
+                        if flag:
+                            r.lrem(key, value, 1)
+                            logger.info("%s del OK alarm msg, key: %s, value: %s" % (d, key, value))
     else:
         logger.info("%s no alarm" % d)
 
